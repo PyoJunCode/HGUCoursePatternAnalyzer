0c75ed0bb4948451edba1592f1353e89f52d977a
JunPyoSeo <21800370@handong.edu>
2019-04-29 00:49:22 +0900

    first

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,22 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	
+	String courseName;
+
+	public static void main(String[] args) {
+		
+
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Course(String Name){
+		
+	}
+
+	public String getCourseName() {
+		
+		
+	}
+}
---
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,20 @@
+package edu.handong.analysis.datamodel;
+
+public class Student {
+	
+	String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Student(String name) {
+		
+	}
+	
+	public String getName() {
+		
+	}
+
+}
